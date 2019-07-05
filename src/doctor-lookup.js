52b0cc16b8ca1dde21b03b10e22ac2ed417152c7
Gavin Kimberlin <guviner@gmail.com>
2019-07-05 16:38:26 -0700

    add the entire html content.

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -3,7 +3,7 @@ export class doctorLookup {
   getdoctor(paragraphs, words) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/practices?location=37.773,-122.413,10&skip=2&limit=10&user_key=' + api_key;
+      let url = 'https://api.betterdoctor.com/2016-03-01/practices?location=37.773,-122.413,10&skip=2&limit=10&user_key=' + apiey;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
