1c9a4f0eec879216be82282f69a903d47b0cac8b
Gavin Kimberlin <guviner@gmail.com>
2019-07-05 16:05:41 -0700

    add link

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,8 +1,9 @@
+import { apiKey } from ./../.env;
 export class doctorLookup {
   getdoctor(paragraphs, words) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=${paragraphs}&words=${words}`;
+      let url = 'https://api.betterdoctor.com/2016-03-01/practices?location=37.773,-122.413,10&skip=2&limit=10&user_key=' + api_key;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
