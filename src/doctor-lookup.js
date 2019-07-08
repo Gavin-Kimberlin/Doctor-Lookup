5206be953505f75d998df3d776e8346f768fa0b5
Gavin Kimberlin <guviner@gmail.com>
2019-07-08 04:16:41 -0700

    get the website working,

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,9 +1,9 @@
-import { apiKey } from './../.env';
+// import { apiKey } from './../.env';
 export class DoctorLookup {
   getDoctor() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=portland&limit=30&user_key=' + apiKey;
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=portland&limit=30&user_key=d5bb2fc15861067df1fb45fba7c95d7c';
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
