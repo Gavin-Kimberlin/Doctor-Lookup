28d89c28abb23553b095605acde6ad6834db698f
josh <joshua.horgen@gmail.com>
2019-07-15 17:56:14 -0700

    fix errors.

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,9 +1,9 @@
 // import { apiKey } from './../.env';
 export class DoctorLookup {
-  getDoctor() {
+  getDoctor(symptom, name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=portland&limit=30&user_key=d5bb2fc15861067df1fb45fba7c95d7c` + ${process.env.exports.apiKey};
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=Portland&user_key=d5bb2fc15861067df1fb45fba7c95d7c`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import $ from 'jquery';
 $(document).ready(function() {
   $('#inputSubmit').click(function() {
     let symptom = $('#medicalIssue').val();
+    let name = $('#name').val();
 
     let doctorLookup = new DoctorLookup();
-    let promise = doctorLookup.getDoctor(symptom);
+    let promise = doctorLookup.getDoctor(symptom, name);
 
     promise.then(function(response) {
       console.log(response);
       let body = JSON.parse(response);
-      $('.showDoctors').html(response);
+      $('.showDoctors').append('<p>List of Doctors:</p>');
+      console.log(body);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
